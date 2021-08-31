94e865056e5ee0a56dd907cf690ff9c1453ba51b
Daniel Reynolds <danielreynolds101@gmail.com>
2021-08-31 12:41:14 -0600

    map is showing

--- src/map/Map.tsx
+++ src/map/Map.tsx
@@ -60,17 +60,24 @@ END OF TERMS AND CONDITIONS
 
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
-import Map from './map/Map';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import './Map.css';
+import 'leaflet/dist/leaflet.css';
 
-function App() {
+function Map() {
     return (
-        <div className="App">
-            <div className="Map">
-                <Map />
-            </div>
-        </div>
+        <MapContainer center={[40.5, -105.5]} zoom={8}>
+            <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+                url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
+            />
+            {/* <Marker position={[51.505, -0.09]}>
+                <Popup>
+                    A pretty CSS3 popup. <br /> Easily customizable.
+                </Popup>
+            </Marker> */}
+        </MapContainer>
     );
 }
 
-export default App;
+export default Map;
